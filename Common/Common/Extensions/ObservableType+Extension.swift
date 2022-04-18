85c7b639bba0f8f37650543b8752f17c8025dd6f
Kinaan <blackshooter@hanmail.net>
2022-04-19 05:25:04 +0900

    feat: Common/asDriverOnErrorJustComplete 오퍼레이터 정의

---
+++ Common/Common/Extensions/ObservableType+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableType+Extension.swift
+//  Common
+//
+//  Created by 오현식 on 2022/04/19.
+//
+
+import RxSwift
+import RxCocoa
+
+extension ObservableType {
+    public func asDriverOnErrorJustComplete() -> Driver<Element> {
+        return asDriver { error in
+            return Driver.empty()
+        }
+    }
+}
