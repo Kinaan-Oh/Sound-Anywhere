d35731f9c8991df7f026b85e2bd981894972747f
Kinaan <blackshooter@hanmail.net>
2022-04-19 03:30:57 +0900

    chore: MapViewModelTests로 재명명

---
+++ Presentation/PresentationTests/MapViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by 오현식 on 2022/04/16.
+//
+
+import XCTest
+import RxTest
+
+class MapViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+}
