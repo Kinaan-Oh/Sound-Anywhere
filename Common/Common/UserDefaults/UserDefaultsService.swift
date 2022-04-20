a0dbeb6df8d6ea1746758cacc6df605aad1bff29
Kinaan <blackshooter@hanmail.net>
2022-04-20 16:52:46 +0900

    fix: 위치권한 UserDefault에서 관리하지 않도록 변경
    위치권한은 자동관리되므로 UserDefault에서 관리할 필요 없음

--- Common/Common/UserDefaults/UserDefaultsService.swift
+++ Common/Common/UserDefaults/UserDefaultsService.swift
@@ -9,13 +9,6 @@ import CoreLocation
 
 final public class UserDefaultsService {
     enum Keys: String {
-        case authorizationStatus
-    }
-    
-    @UserDefault(key: Keys.authorizationStatus.rawValue, defaultValue: CLAuthorizationStatus.notDetermined.rawValue)
-    public static var authorizationStatusRawValue: CLAuthorizationStatus.RawValue
-    public static var authorizationStatus: CLAuthorizationStatus {
-        get { CLAuthorizationStatus.init(rawValue: authorizationStatusRawValue)! }
-        set { authorizationStatusRawValue = newValue.rawValue }
+        case favoriteZones
     }
 }
