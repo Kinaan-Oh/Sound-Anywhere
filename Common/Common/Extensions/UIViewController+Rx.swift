656e17b8de693487697119c5a52a8808141f6f28
Kinaan <blackshooter@hanmail.net>
2022-04-20 17:13:04 +0900

    feat: UIViewController+Rx 구현

---
+++ Common/Common/Extensions/UIViewController+Rx.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewController+Rx.swift
+//  Common
+//
+//  Created by 오현식 on 2022/04/20.
+//
+
+// Reference: https://github.com/devxoul/RxViewController/blob/master/Sources/RxViewController/UIViewController%2BRx.swift
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+public extension Reactive where Base: UIViewController {
+  var viewDidLoad: ControlEvent<Void> {
+    let source = self.methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
+    return ControlEvent(events: source)
+  }
+
+  var viewWillAppear: ControlEvent<Bool> {
+    let source = self.methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
+    
+  var viewDidAppear: ControlEvent<Bool> {
+    let source = self.methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
+
+  var viewWillDisappear: ControlEvent<Bool> {
+    let source = self.methodInvoked(#selector(Base.viewWillDisappear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
+    
+  var viewDidDisappear: ControlEvent<Bool> {
+    let source = self.methodInvoked(#selector(Base.viewDidDisappear)).map { $0.first as? Bool ?? false }
+    return ControlEvent(events: source)
+  }
+}
