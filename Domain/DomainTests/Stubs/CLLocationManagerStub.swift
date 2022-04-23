3722a73a88f2d6c64eb79d399b153a38d349acb1
Kinaan <blackshooter@hanmail.net>
2022-04-23 11:04:18 +0900

    style: Domain/CLLocationManagerStub ReturnValue 정의

--- Domain/DomainTests/Stubs/CLLocationManagerStub.swift
+++ Domain/DomainTests/Stubs/CLLocationManagerStub.swift
@@ -11,11 +11,14 @@ import Common
 import RxSwift
 
 final class CLLocationManagerStub: CLLocationManagerQuerying {
+    var observeAuthorizationStatus_ReturnValue: Observable<CLAuthorizationStatus> = Observable.just(.notDetermined)
+    var observeLocation_ReturnValue: Observable<CLLocation?> = Observable.just(nil)
+    
     func observeAuthorizationStatus() -> Observable<CLAuthorizationStatus> {
-        return Observable.just(.notDetermined)
+        return observeAuthorizationStatus_ReturnValue
     }
     
     func observeLocation() -> Observable<CLLocation?> {
-        return Observable.just(nil)
+        return observeLocation_ReturnValue
     }
 }
