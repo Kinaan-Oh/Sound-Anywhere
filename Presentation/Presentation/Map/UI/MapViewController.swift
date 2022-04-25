dbae6a691e190094f9bbfa7ca18982cbef79e915
Kinaan <blackshooter@hanmail.net>
2022-04-25 16:23:15 +0900

    feat: Presentation/currentLocationButton 바인딩, output.location에 throttle 적용
    위치변화가 급격한 경우(차량, 비행기 등)의 상황을 고려하여 location에 throttle 적용.

--- Presentation/Presentation/Map/UI/MapViewController.swift
+++ Presentation/Presentation/Map/UI/MapViewController.swift
@@ -9,7 +9,6 @@ import MapKit
 import UIKit
 
 import Common
-import DIContainer
 import Domain
 import RxCocoa
 import RxSwift
@@ -33,6 +32,7 @@ final class MapViewController: UIViewController {
         super.viewDidLoad()
         configureLocation()
         bindViewModel()
+        bindUI()
     }
     
     // MARK: - Helpers
@@ -47,31 +47,38 @@ final class MapViewController: UIViewController {
     }
     
     private func bindViewModel() {
-        let viewDidAppearEvent = self.rx.viewDidAppear
-            .asDriver()
+        let viewDidAppearEvent = rx.viewDidAppear.asDriver()
         
         let input = MapViewModel.Input(viewDidAppearEvent: viewDidAppearEvent)
         let output = viewModel.transform(input: input)
         
         output.authorizationStatus
             .drive { [weak self] authorizationStatus in
-                guard let self = self,
-                      authorizationStatus == .authorizedWhenInUse
-                else { return }
-                self.viewModel.startUpdatingLocation()
+                guard let self = self else { return }
+                switch authorizationStatus {
+                case .authorizedWhenInUse:
+                    self.viewModel.startUpdatingLocation()
+                default:
+                    self.viewModel.stopUpdatingLocation()
+                }
             }
             .disposed(by: disposeBag)
         
         output.location
-            .drive { [weak self] location in
-                guard let self = self,
-                      let location = location
-                else { return }
+            .throttle(.milliseconds(300), latest: true)
+            .drive { location in
+                guard let location = location else { return }
                 UserDefaultsService.recentLocation = location
             }
             .disposed(by: disposeBag)
     }
     
     private func bindUI() {
+        currentLocationButton.rx.tap
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] in
+                self?.configureLocation()
+            })
+            .disposed(by: disposeBag)
     }
 }
