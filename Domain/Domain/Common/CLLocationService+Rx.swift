19519391af02f3fd28a5c844478fec7d09cea9fa
Kinaan <blackshooter@hanmail.net>
2022-05-04 01:16:28 +0900

    feat: Domain/CLLocationService+Rx, CLLocationservice 구현

---
+++ Domain/Domain/Common/CLLocationService+Rx.swift
@@ -0,0 +1,32 @@
+//
+//  CLLocationService+Rx.swift
+//  Domain
+//
+//  Created by 오현식 on 2022/05/02.
+//
+
+import CoreLocation
+
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: CLLocationService {
+    var authorizationStatus: ControlEvent<CLAuthorizationStatus> {
+        var source: Observable<CLAuthorizationStatus>
+        
+        if #available(iOS 14.0, *) {
+            source = self.methodInvoked(#selector(Base.locationManagerDidChangeAuthorization(_:)))
+                .map { ($0.first as? CLLocationManager)?.authorizationStatus ?? CLAuthorizationStatus.notDetermined }
+        } else {
+            source = self.methodInvoked(#selector(Base.locationManager(_:didChangeAuthorization:)))
+                .map { $0[1] as? CLAuthorizationStatus ?? CLAuthorizationStatus.notDetermined }
+        }
+        return ControlEvent(events: source)
+    }
+    
+    var location: ControlEvent<CLLocation?> {
+        let source = self.methodInvoked(#selector(Base.locationManager(_:didUpdateLocations:)))
+            .map { ($0[1] as? [CLLocation])?.last ?? nil }
+        return ControlEvent(events: source)
+    }
+}
---
+++ Domain/Domain/Common/CLLocationService.swift
@@ -0,0 +1,85 @@
+//
+//  CLLocationManager+Extension.swift
+//  Domain
+//
+//  Created by 오현식 on 2022/05/02.
+//
+
+import CoreLocation
+
+import RxSwift
+
+// MARK: - CLLocationService
+
+public final class CLLocationService: NSObject {
+    private let locationManager = CLLocationManager()
+    
+    public override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension CLLocationService: CLLocationManagerDelegate {
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    }
+    
+    public func locationManager(
+        _ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+    }
+    
+    public func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+    }
+    
+    public func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+    }
+}
+
+// MARK: - CLLocationServiceCommanding
+
+public protocol CLLocationServiceCommanding {
+    func requestWhenInUseAuthorization()
+    func startUpdatingLocation()
+    func stopUpdatingLocation()
+}
+
+extension CLLocationService: CLLocationServiceCommanding {
+    public func requestWhenInUseAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    public func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    public func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+// MARK: - CLLocationServiceQuerying
+
+public protocol CLLocationServiceQuerying {
+    func observeAuthorizationStatus() -> Observable<CLAuthorizationStatus>
+    func observeLocation() -> Observable<CLLocation?>
+}
+
+extension CLLocationService: CLLocationServiceQuerying {
+    public func observeAuthorizationStatus() -> Observable<CLAuthorizationStatus> {
+        return rx.authorizationStatus.asObservable()
+    }
+    
+    public func observeLocation() -> Observable<CLLocation?> {
+        return rx.location.asObservable()
+    }
+}
