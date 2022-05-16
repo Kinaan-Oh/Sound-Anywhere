d2a7aec542cf48c5b93be9879230a85cbb790a41
Kinaan <blackshooter@hanmail.net>
2022-05-16 16:03:20 +0900

    feat: RxTestPackage/ Nimble, Quick, RxNimble 의존성 추가
    RxNimble은 Nimble, RxSwift 의존성을 갖기 때문에 마찬가지로 RxSwift 중복적재 문제 발생 가능.
    따라서, RxTest와 같은 방법으로 RxNimble 소스 복사로 해결.

---
+++ RxTestPackage/RxTestPackage/RxNimble/Core/Expectation+Ext.swift
@@ -0,0 +1,26 @@
+//
+//  Expectation+Ext.swift
+//  RxTestPackage
+//
+//  Created by 오현식 on 2022/05/16.
+//
+
+import Nimble
+
+extension Expectation {
+    #if swift(>=4.1)
+    #else
+    init(_ expression: Expression<T>) {
+        self.expression = expression
+    }
+    #endif
+
+    internal func transform<U>(_ closure: @escaping (T?) throws -> U?) -> Expectation<U>{
+        let exp = expression.cast(closure)
+        #if swift(>=4.1)
+        return Expectation<U>(expression: exp)
+        #else
+        return Expectation<U>(exp)
+        #endif
+    }
+}
