37e89d57e895aabef39df4d1420fcfdf7f840078
Kinaan <blackshooter@hanmail.net>
2022-05-31 22:40:25 +0900

    chore: Presentation,Resource/ 불필요한 코드 정리

--- Resource/Resource/Extensions/Resource+Extension/Resource+Storyboard.swift
+++ Resource/Resource/Extensions/Resource+Extension/Resource+Storyboard.swift
@@ -12,14 +12,7 @@ import Common
 extension Resource {
     public final class Storyboard {
         public enum ID: String {
-            case main
-            case home
-            case search
             case map
-            case record
-            case login
-            case signUp
-            case emailSignUp
             
             public var stringValue: String {
                 return self.rawValue.firstCharCapitalized
