c37e7036d342ff11c4c8b5b8d7ecf9f12db98b10
Kinaan <blackshooter@hanmail.net>
2022-07-01 00:55:26 +0900

    refactor: Resource/MapStoryboard Module 속성을 UI 모듈로 변경
    Resource 모듈 MapStoryboard Module 속성을 UI 모듈의 MapViewController로 변경. (기존 Presentation 모듈 내 MapViewController를 UI 모듈로 분리하여 생긴 문제 해결)

--- Resource/Resource/Extensions/Resource+Extension/Resource+Storyboard.swift
+++ Resource/Resource/Extensions/Resource+Extension/Resource+Storyboard.swift
@@ -18,5 +18,20 @@ extension Resource {
                 return self.rawValue.firstCharCapitalized
             }
         }
+        
+        private let id: String
+        private let storyboard: UIStoryboard
+        
+        public init(id: ID) {
+            self.id = id.stringValue
+            storyboard = UIStoryboard(name: id.stringValue, bundle: Resource.bundle)
+        }
+        
+        public func instance<T: UIViewController>() -> T {
+            guard let instance = storyboard.instantiateViewController(withIdentifier: id) as? T else {
+                fatalError("Resource.Storyboard.instance Failed!")
+            }
+            return instance
+        }
     }
 }
